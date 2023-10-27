68255385882f90f2a24b098a1bcc6e3488a040da
Nikita <zalesov.anatoly@yandex.ru>
2023-10-27 13:58:34 +0400

    added coments

--- task_advanced.sql
+++ task_advanced.sql
@@ -1,3 +1,12 @@
+/*
+ЗАДАЧА:
+Вывести: Code, ITEM1.ID_Group, ITEM1.Value и ITEM2.ID_Group, ITEM2.Value, сопоставленные по группам по следующим правилам:
+1) элементы между группами сопоставляются по полю Code (т.е. всегда ITEM1.Code = ITEM2.Code)
+2) группы элементов в ITEM1 сопоставляются с элементами группы в ITEM2 по следующему правилу:
+	если в соответсвующей записи таблицы GROUP (связь с ITEMx по идентификатору ID_Group) флаг ByGroup = 1
+		то: ITEM1 и ITEM2 сопоставляются по ID_Group, Code
+		иначе: необходимо сопоставить группы и все их элементы из двух таблиц по значению Value в главном элементе
+*/
 SELECT Item1.Code, ITEM1.ID_Group, ITEM1.Value, ITEM2.ID_Group, ITEM2.Value
   FROM Item1
   JOIN Item2 ON Item1.Code = Item2.Code
