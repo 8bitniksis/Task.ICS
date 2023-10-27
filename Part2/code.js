d60dc56a5d58aeed0d9bad8d2ae208d230f38381
Nikita <zalesov.anatoly@yandex.ru>
2023-10-27 18:42:53 +0400

    Update code.js
    Replace spaces on '|'

--- Part2/code.js
+++ Part2/code.js
@@ -6,99 +6,99 @@
  
 // ПРИМЕР ДАННЫХ
 var tree = {
-   left: {
-       left: "Первое предложение из произвольной строки",
-       right: {
-           left: "Другое произвольное предложение",
-           right: "Еще одно следующее предложение, но не очень длинное"
-       }
-   },
-   right: {
-       left: {
-           left: {
-               left: "Еще одно не очень длинное предложение",
-               right: ""
-           }, 
-           right: {
-               left: "",
-               right: "Еще одно не очень длинное предложение"
-           }
-       },
-       right: {
-           left: {
-               left: "Предложение",
-               right: "Еще одно следующее предложение, но не очень длинное"
-           },
-           right: {
-               left: "Другое произвольное предложение",
-               right: {
-                   left: "Два слова",
-                   right: "Еще одно следующее предложение, но не очень длинное"
-               }
-           }
-       }
-   }
+  left: {
+      left: "Первое предложение из произвольной строки",
+      right: {
+          left: "Другое произвольное предложение",
+          right: "Еще одно следующее предложение, но не очень длинное"
+      }
+  },
+  right: {
+      left: {
+          left: {
+              left: "Еще одно не очень длинное предложение",
+              right: ""
+          }, 
+          right: {
+              left: "",
+              right: "Еще одно не очень длинное предложение"
+          }
+      },
+      right: {
+          left: {
+              left: "Предложение",
+              right: "Еще одно следующее предложение, но не очень длинное"
+          },
+          right: {
+              left: "Другое произвольное предложение",
+              right: {
+                  left: "Два слова",
+                  right: "Еще одно следующее предложение, но не очень длинное"
+              }
+          }
+      }
+  }
 };
- 
+
 // ЗДЕСЬ НАПИСАТЬ КОД. РЕЗУЛЬТАТ ВЫВЕСТИ ЧЕРЕЗ console.log
 readTree(tree);
 function readTree(obj,path)
 {
-  if (isNode(obj))
-  {
-      if (typeof path == 'undefined')
-        path=''
-      if (path != '')
-        path=path+'->'; 
- 
-      readTree(obj.left, path+'Left');
-      readTree(obj.right, path+'Right');
-  } 
-  else if (isList(obj))
-  {
-      console.log(path + ': ' + findLongWords(obj,3))
-  }
-  return null;
+ if (isNode(obj))
+ {
+     if (typeof path == 'undefined')
+       path=''
+     if (path != '')
+       path=path+'->'; 
+
+     readTree(obj.left, path+'Left');
+     readTree(obj.right, path+'Right');
+ } 
+ else if (isList(obj))
+ {
+     console.log(path + ': ' + findLongWords(obj,3))
+ }
+ return null;
 }
- 
+
 function isList(obj){
-  if (typeof(obj)=="string")
-    return true;
-  else
-    return false;  
+ if (typeof(obj)=="string")
+   return true;
+ else
+   return false;  
 }
- 
+
 function isNode(obj)
 {
-  if (typeof(obj)=="string")
-    return false;
-  else
-    return true;  
+ if (typeof(obj)=="string")
+   return false;
+ else
+   return true;  
 }
- 
- 
+
+
 function longer(champ, contender) {
-  return (contender.length > champ.length) ? contender: champ;
+ return (contender.length > champ.length) ? contender: champ;
 }
- 
+
 function findLongWords(str, word_count) {
- 
-    str = str.replace(/[.,!?;|<>]+/g, '');
- 
-    var words = str.split(' ');
-    var result='';
-    for (var i=1;i<=word_count;i++)
-    {
-        if ((words.length===0) || ((words.length===1) && (words[0]==='')))
-          continue;
- 
-        var theLongestWord=words.reduce(longer);
-        result = result + theLongestWord+' ';  
- 
-        var word = words.indexOf(theLongestWord);
-        if(word != -1) {
-          words.splice(word, 1);
-        }
-    }
-    return result;
+
+   str = str.replace(/[.,!?;|<>]+/g, '');
+
+   var words = str.split(' ');
+   var result='';
+   for (var i=1;i<=word_count;i++)
+   {
+       if ((words.length===0) || ((words.length===1) && (words[0]==='')))
+         continue;
+
+       var theLongestWord=words.reduce(longer);
+       result = result + '|' + theLongestWord;  
+
+       var word = words.indexOf(theLongestWord);
+       if(word != -1) {
+         words.splice(word, 1);
+       }
+   }
+   return result;
 }
